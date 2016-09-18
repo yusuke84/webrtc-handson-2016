f4ee144dd8a31a8d046534df599c6c5029a258f5
yusuke84 <com.y.naka@gmail.com>
2016-09-18 09:12:01 +0900

    added gUM function

--- client/webrtc.js
+++ client/webrtc.js
@@ -4,3 +4,15 @@ let localStream = null;
 let peerConnection = null;
 let textForSendSdp = document.getElementById('text_for_send_sdp');
 let textToReceiveSdp = document.getElementById('text_for_receive_sdp');
+
+// start local video
+function startVideo() {
+    navigator.mediaDevices.getUserMedia({video: true, audio: true})
+        .then(function (stream) { // success
+            localStream = stream;
+            localVideo.src = window.URL.createObjectURL(localStream);
+        }).catch(function (error) { // error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+}
