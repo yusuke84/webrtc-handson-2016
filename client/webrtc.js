e777a044dd3d18ba47c9a771998be373b4628011
yusuke84 <com.y.naka@gmail.com>
2016-09-18 10:33:25 +0900

    making step2

--- client/webrtc.js
+++ client/webrtc.js
@@ -5,6 +5,9 @@ let peerConnection = null;
 let textForSendSdp = document.getElementById('text_for_send_sdp');
 let textToReceiveSdp = document.getElementById('text_for_receive_sdp');
 
+// 互換性対応
+RTCPeerConnection = window.RTCPeerConnection || window.webkitRTCPeerConnection;
+
 // getUserMediaでカメラ、マイクにアクセス
 function startVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: true})
@@ -12,7 +15,7 @@ function startVideo() {
             playVideo(localVideo,stream);
             localStream = stream;
         }).catch(function (error) { // error
-        console.error('mediaDevice.getUserMedia() error:', error);
+            console.error('mediaDevice.getUserMedia() error:', error);
         return;
     });
 }
@@ -26,4 +29,29 @@ function playVideo(element, stream) {
         element.src = window.URL.createObjectURL(stream);
     }
     element.play();
+}
+
+// Connectボタンが押されたら処理を開始
+function connect() {
+    if (! peerConnection) {
+        console.log('make Offer');
+        makeOffer();
+    }
+    else {
+        console.warn('peer already exist.');
+    }
+}
+
+// Offer SDPを生成する
+function makeOffer() {
+    peerConnection = prepareNewConnection();
+    peerConnection.createOffer()
+        .then(function (sessionDescription) {
+            console.log('createOffer() succsess in promise');
+            return peerConnection.setLocalDescription(sessionDescription);
+        }).then(function() {
+            console.log('setLocalDescription() succsess in promise');
+    }).catch(function(err) {
+        console.error(err);
+    });
 }
