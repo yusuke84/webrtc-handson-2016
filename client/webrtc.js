c62bb706f7d2ca9ddcab032bac67ea06f4c8e0c5
Yusuke Naka <com.y.naka@gmail.com>
2017-04-09 07:59:30 +0900

    Update webrtc.js

--- client/webrtc.js
+++ client/webrtc.js
@@ -41,10 +41,9 @@ function prepareNewConnection() {
     peer.onicecandidate = function (evt) {
         if (evt.candidate) {
             console.log(evt.candidate);
-            sendIceCandidate(evt.candidate);
         } else {
             console.log('empty ice event');
-            // sendSdp(peer.localDescription);
+            sendSdp(peer.localDescription);
         }
     };
 
