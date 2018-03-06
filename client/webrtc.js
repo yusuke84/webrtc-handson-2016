69d1d516f88cb09713c90fb9b876cdffd9f0c243
yusuke84 <com.y.naka@gmail.com>
2018-03-06 14:03:24 +0900

    update handson_step1 branch

--- client/webrtc.js
+++ client/webrtc.js
@@ -1,29 +1,24 @@
-let localVideo = document.getElementById('local_video');
-let remoteVideo = document.getElementById('remote_video');
+const localVideo = document.getElementById('local_video');
+const remoteVideo = document.getElementById('remote_video');
+const textForSendSdp = document.getElementById('text_for_send_sdp');
+const textToReceiveSdp = document.getElementById('text_for_receive_sdp');
 let localStream = null;
 let peerConnection = null;
-let textForSendSdp = document.getElementById('text_for_send_sdp');
-let textToReceiveSdp = document.getElementById('text_for_receive_sdp');
+let negotiationneededCounter = 0;
+let isOffer = false;
 
 // getUserMediaでカメラ、マイクにアクセス
-function startVideo() {
-    navigator.mediaDevices.getUserMedia({video: true, audio: true})
-        .then(function (stream) { // success
-            playVideo(localVideo,stream);
-            localStream = stream;
-        }).catch(function (error) { // error
-            console.error('mediaDevice.getUserMedia() error:', error);
-        return;
-    });
+async function startVideo() {
+    try{
+        localStream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+        playVideo(localVideo,localStream);
+    } catch(err){
+        console.error('mediaDevice.getUserMedia() error:', err);
+    }
 }
 
 // Videoの再生を開始する
-function playVideo(element, stream) {
-    if ('srcObject' in element) {
-        element.srcObject = stream;
-    }
-    else {
-        element.src = window.URL.createObjectURL(stream);
-    }
-    element.play();
+async function playVideo(element, stream) {
+    element.srcObject = stream;
+    await element.play();
 }
